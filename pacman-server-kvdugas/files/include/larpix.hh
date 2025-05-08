7153b9f1d3fa13cee7e5655ce0f828c1ba587950
Katherine Dugas <91695599+kvdugas@users.noreply.github.com>
2025-05-08 11:34:32 -0700

    Update larpix.hh

--- pacman-server-kvdugas/files/include/larpix.hh
+++ pacman-server-kvdugas/files/include/larpix.hh
@@ -1,7 +1,7 @@
 #ifndef larpix_hh
 #define larpix_hh
 
-#define LARPIX_PACKET_LEN 16 // bytes
+#define LARPIX_PACKET_LEN 32 // bytes
 
 #define WORD_TYPE_OFFSET 0 // bytes
 #define IO_CHANNEL_OFFSET 1 // bytes
